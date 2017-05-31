bd9fe2faff4b2a0ce9743c7c49a85a65c48c992f
Spencer Ward <spencertward@gmail.com>
2017-05-31 15:19:43 -0600

    first commit

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Books from '../imports/ui/containers/Books';
+
+render(
+  <Books />,
+  document.getElementById('react-root')
+);
